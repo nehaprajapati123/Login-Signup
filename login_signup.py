131dadb40c03f5cc2f930f9dcd1a8fe79b9cdc9c
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2021-12-29 16:11:40 +0530

    Update login_signup.py

--- login_signup.py
+++ login_signup.py
@@ -1,6 +1,6 @@
 import json
-user=int(input("Press 1 to Login or Press 2 to Signup:"))
-# Signup
+user=int(input("Press 1 for login or Press 2 for signup:"))
+# Signup:-
 if user==2:
     name=input("Enter username:")
     password =input("Enter strong password:")
@@ -14,13 +14,14 @@ if user==2:
             if (i.islower()):
                 lower=1
             if(i=='@'or i=='$' or i=='_' or i=='#' or i=="!" or i=="%" or i=="^" or i=="&" or i=="*" or i=="(" or i==")"):
+
                 special=1
         total=upper+digit+lower+special 
-        if total!=6:
-            print("your passward must contain 1 capital letter, 1 small letter, 1 special character, 1 digit and length of passward must be 8 ")
+        if total!=4:
+            print("Please use  atleast one capital letter , Smalll letter, Digit and Special character!! ")
         else:
-            password2=input("Conform Password:")
-            if password!=password2:
+            confirm=input("Confirm Password:")
+            if password!=confirm:
                 print("Please enter correct pasword!")
             else:
                 hobby=input("Enter your Hobbies:")
@@ -45,31 +46,10 @@ if user==2:
                 print("**Congrats",name,"You are signed up successfully**")
     else:
         print("Password is too short!")   
-#login
+# login:-
 elif user==1:
-    name1=input("Enter your name:")
-    passw=input("Enter your Password:")
-    upper,lower,digit,special=0,0,0,0
-    if len(passw)>=6:
-        for i in passw:
-            if (i.isupper()):
-                upper=1
-            if (i.isdigit()):
-                digit=1
-            if (i.islower()):
-                lower=1
-            if(i=='@'or i=='$' or i=='_' or i=='#' or i=="!" or i=="%" or i=="^" or i=="&" or i=="*" or i=="(" or i==")"):
-                special=1
-        total=upper+digit+lower+special  
-       
-        if total==4:
-            print("Password is submitted successfully")
-
-        else:
-            print("Please use  atleast one capital letter , Smalll letter, Digit and Special character!! ")
-        
-    else:
-        print("Password is too short!")
+    name1=input("Enter your name: ")
+    passw=input("Enter your Password: ")
     r=open("neha.json","r")
     j=r.read()
     if name1 in j:
